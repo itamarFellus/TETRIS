5cad78ee5b750e22879afd8442bc6b2f4558d4ac
itamarFellus <itamar.fellus@gmail.com>
2019-11-04 10:34:33 +0200

    Initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="container">
+      hello world
+      </div>
+    )
+  }
+}
